c8ccff6afc28f4f512aaf3c4eee67f34b2300464
brluizfelippe <l.felippe@terra.com.br>
2017-10-31 12:02:18 -0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ hi: 'there Git, I like it!!' });
 });
 
 const PORT = process.env.PORT || 5000;
