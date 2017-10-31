0c866b1a6a90ec793f82d9833e74578cce7b74f2
Luiz Felippe <contato@lfelippe.com.br>
2017-10-31 12:41:12 -0200

    testing index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+const express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there thanks!' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
