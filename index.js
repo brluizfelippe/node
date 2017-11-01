91ee8ae7342977a3465bbbb86e104787fbef587d
Luiz Felippe <contato@lfelippe.com.br>
2017-11-01 14:42:03 -0200

    testing google oauth

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-// JavaScript source code
 const express = require('express');
-var app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'budy' });
-});
+const app = express();
+
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        accessToken => {
+            console.log(accessToken);
+        }
+    )
+);
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
